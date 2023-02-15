4dabca9bee6ddc5e2090e1d6ad9b121f82710fb7
buddha <buddha@DESKTOP-2QGI15I>
2023-02-15 22:44:51 +0700

    Добавьте файлы проекта.

---
+++ Micran_Test_Task_2.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    char split;
+    int length;
+    int count = 0;
+    string result = "";
+
+    cin >> split;
+
+    cin >> length;
+
+    vector<int> numbers(length);
+    for (int i = 0; i < length; i++)
+    {
+        cin >> numbers[i];
+    }
+
+    int lastNumber = numbers.back();
+
+    for (int i = 0; i < length; i++)
+    {
+        if (numbers[i] > lastNumber)
+        {
+            count++;
+            result += to_string(numbers[i]) + split;
+        }
+    }
+
+    if (count == 0) {
+        cout << count << endl;
+    }
+    else {
+        cout << count << endl;
+        cout << result.substr(0, result.size() - 1) << endl;
+    }
+
+    return 0;
+}
